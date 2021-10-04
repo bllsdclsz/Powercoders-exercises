67303dff446d05be31600b54de24f8afeda7f1ca
ekolog <ekolog055@gmail.com>
2021-10-04 09:25:59 +0200

    .

--- JS_week3_Intro/counting chars/app.js
+++ JS_week3_Intro/counting chars/app.js
@@ -1,9 +1,14 @@
 let userInput = prompt("Please type something!");
 let lengthOfInput = userInput.length;
-let output = `Our input is ${userInput} and has ${lengthOfInput} characters.`;
+let counter = 0;
 
-if(userInput != null && userInput != ""){
-   console.log(output);
-}else{
-   console.log("You didn't type anything");
-}
+userInput.split("").forEach((e) => {
+   if(e === ' '){
+      counter
+   } else {
+      counter++
+   }
+});
+
+let output = `Our input is ${userInput} and has ${counter} characters.`;
+console.log(output)
---
+++ JS_week3_Intro/printing quotes/app.js
@@ -0,0 +1,5 @@
+let author = prompt("What is the author's name?")
+let quote = prompt("What is the quote?")
+let output = `${author.toUpperCase()} says, "${quote}"`
+
+console.log(output)
