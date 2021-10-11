1484a4213fdae41cdd501a49a728eec24e7310f3
ekolog <ekolog055@gmail.com>
2021-10-11 16:42:17 +0200

    password added

--- JS_week5/monday/app.js
+++ JS_week5/monday/app.js
@@ -1,50 +1,103 @@
 const PASSWORD = "password";
-let userPassword = "passwor"
-function isPasswordCorrect(pUserPassword) {
-   if(PASSWORD==pUserPassword) {
+
+let body = document.body;
+let list = document.querySelector("ul");
+let input = document.querySelector("input[type='text']");
+let form = document.querySelector("form");
+let idCounter = 3;
+
+isPasswordCorrect()
+
+function isPasswordCorrect() {
+   let userPassword = prompt("Please enter your password!")
+   console.log(userPassword)
+   if(PASSWORD==userPassword) {
       return true;
    } else {
-      console.log("Your password is incorrect")
+      alert("Your password is incorrect")
+      isPasswordCorrect()
    }
 }
 
-function todoItem (pTodo, pIsChecked = false) {
+function todoItem(pId, pTodo, pIsDone = false) {
+   this.id = pId;
    this.todo = pTodo;
-   this.isChecked = pIsChecked;
-};
-
-let todoArray = [
-   new todoItem("go shopping"),
-   new todoItem("do your homeworks"),
-   new todoItem("clean the room")
-];
-
-addTodo("drink a tea", true);
-addTodo("adjust your schedule");
-sortArray(todoArray);
-if(isPasswordCorrect(userPassword)) {
-   printArray(todoArray);
-} else {
-   return false;
+   this.isDone = pIsDone;
 }
 
-function sortArray(pArray) {
-   return pArray.sort((a, b) => {
+let todoArray = [new todoItem(1, "go shopping"), new todoItem(2, "do your homeworks"), new todoItem(3, "clean the room")];
+
+function sortArray() {
+   return todoArray.sort((a, b) => {
       return a.todo < b.todo ? -1 : a.todo > b.todo ? 1 : 0;
    });
 }
 
 function addTodo(pTodo, pIsChecked) {
-   return todoArray.push(new todoItem(pTodo, pIsChecked));
+   idCounter++;
+   return todoArray.push(new todoItem(idCounter, pTodo, pIsChecked));
 }
 
-function printArray(pArray) {
-   for (let index = 0; index < pArray.length; index++) {
-      console.log(`${pArray[index].todo} --> ${pArray[index].isChecked}`);
+function printArray() {
+   for (let index = 0; index < todoArray.length; index++) {
+      let todoItem = todoArray[index];
+      let todoItemElement = document.createElement('li')
+      todoItemElement.id = todoItem.id
+      todoItemElement.innerText = `${todoItem.todo} --> ${todoItem.isDone}`
+      list.appendChild(todoItemElement)
+      addColorToDoneTodos(todoArray[index]);
+   }
+   let listItems = document.querySelectorAll("li");
+   for (let i = 0; i < listItems.length; i++) {
+      listItems[i].addEventListener("click", (event) => {
+         event.preventDefault();
+         let elementId = event.target.id;
+         console.log(event.target)
+         searchAndChangeStatus(elementId);
+      });
+   }
+}
+
+function searchAndChangeStatus(pElementId) {
+   for (let index = 0; index < todoArray.length; index++) {
+      if (pElementId == todoArray[index].id) {
+         changeIsDoneStatus(todoArray[index]);
+         refreshInnerHtml(list);
+         printArray();
+      }
    }
 }
 
 function changeIsDoneStatus(pTodo) {
-   return pTodo.isChecked ? pTodo.isChecked = false : pTodo.isChecked = true
+   return pTodo.isDone ? (pTodo.isDone = false) : (pTodo.isDone = true);
 }
 
+function takeUserInputValue() {
+   if (input.value == "" || input.value == null || input.value == undefined) {
+      alert("Your todo is invalid. Please try again");
+      console.log("input.value not valid");
+      return false;
+   } else {
+      console.log(input.value);
+      return input.value;
+   }
+}
+
+function refreshInnerHtml(pInnerHtml) {
+   return (pInnerHtml.innerHTML = "");
+}
+
+form.addEventListener("submit", (event) => {
+   event.preventDefault();
+   refreshInnerHtml(list);
+   let value = takeUserInputValue();
+   if (value) {
+      addTodo(value);
+      sortArray();
+      printArray();
+   }
+});
+
+function addColorToDoneTodos(pTodo) {
+   return pTodo.isDone ? (document.getElementById(`${pTodo.id}`).classList.add("done")) : (document.getElementById(`${pTodo.id}`).classList.remove("done"));
+}
